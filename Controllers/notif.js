c8c0f7270c707ecf8543f77a97bda661ae1531ab
Aaditya1998 <61281583+Aaditya1998@users.noreply.github.com>
2023-09-02 15:51:16 +0530

    Good

--- Controllers/notif.js
+++ Controllers/notif.js
@@ -3,14 +3,27 @@ import Notif from "../models/notif.js";
 
 export const createNotif = async (req, res) => {
   const { description } = req.body;
-  const newNotif = new Notif({
-    description,
-  });
+
   try {
+    // Check if a notification with the same description already exists
+    const existingNotif = await Notif.findOne({ description });
+
+    if (existingNotif) {
+      // Handle conflict - a notification with the same description already exists
+      return res.status(409).json({ message: "Notification already exists." });
+    }
+
+    // If no conflict, create and save the new notification
+    const newNotif = new Notif({ description });
     await newNotif.save();
+
+    // Send in-app notification
     await inAppNotification(description, "64f23ae4bfc062112c128dfc");
+
+    // Respond with the newly created notification
     res.status(201).json(newNotif);
   } catch (error) {
-    res.status(409).json({ message: error });
+    // Handle other errors gracefully
+    res.status(500).json({ message: "Server error." });
   }
 };
